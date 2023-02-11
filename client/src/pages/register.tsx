83765c463a82ae478cf3d20b016b0c250bd06824
Simeon Cholakov <simeonstefanovcholakov@gmail.com>
2023-02-11 11:36:50 +0200

    register routing

---
+++ client/src/pages/register.tsx
@@ -0,0 +1,10 @@
+import Form from '@/components/register-page/register-form/Form'
+import React from 'react'
+
+const register = () => {
+    return (
+        <Form />
+    )
+}
+
+export default register
