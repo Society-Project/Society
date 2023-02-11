a1474a91503dc749c0c0aa28cd3c8ecd96c82d63
di-yordanova <di.gyordanova@gmail.com>
2023-02-11 10:42:49 +0200

    Login page with image

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
-import {Box, Button, TextField, Typography} from '@mui/material'
+import {Box, Button, TextField, Typography } from '@mui/material'
+import laptopPicture from "/src/images/laptopPicture.jpg";
+import headerLogo from "/src/images/headerLogo.png"
+import styled from "styled-components";
+
+
 
 const Login = () => {
   return (
-    <div>
-      <form>
-            <Box 
+    <div className='wrapper'>
+        <Logo src={laptopPicture.src} alt="Picture" />
+
+          <Box 
                 display="flex" 
                 flexDirection={"column"} 
                 maxWidth={381}
@@ -15,14 +21,18 @@ const Login = () => {
                 marginTop={6}
                 padding={10}
                 borderRadius={5}
-                boxShadow={'5px 5px 5px 5px #ccc'}    
-                color={"red"}                  
-                >                
+                boxShadow={'3px 13px 80px rgba(0, 0, 0, 0.25);'}    
+                color={"red"} 
+                >
+                {/* <img src='https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1472&q=80' width="500" height="600" alt=''/> */}
                 <Typography 
                 variant="h2" 
                 padding={3} 
                 textAlign='center'
                 >
+                <img src={headerLogo.src} alt="HeaderLogo" 
+                width= "434px" 
+                height= "241px"/>
                   Login
                   </Typography>
                 <TextField 
@@ -86,15 +96,26 @@ const Login = () => {
                     sx={{marginTop:3, borderRadius:10, 
                     backgroundColor:"white",
                     color:"black",
-                    }}
+                  }}
                     variant='contained'
                     color='success'>
                     Sign Up
                     </Button>
-            </Box>
-      </form>
+          </Box>
+
     </div>
   )
 }
+const Logo = styled.img`
+  height: 350px;
+  width: 200px;
+  opacity: 0.9;
+  border-radius"0% 10% 10% 0%"
 
+
+  :hover {
+    cursor: pointer;
+    opacity: 1;
+  }
+`;
 export default Login
