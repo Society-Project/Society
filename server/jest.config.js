22d3e9816833f8bddd137a097d658fa0ff834e3e
beval1 <bobo91280@gmail.com>
2023-02-11 22:38:14 +0200

    server folder clean up

--- server/jest.config.js
+++
@@ -1,8 +0,0 @@
-export default {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    transform: {
-      '^.+\\.ts?$': 'ts-jest',
-    },
-    transformIgnorePatterns: ['./node_modules/'],
-  };
