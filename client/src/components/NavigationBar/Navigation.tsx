7808743cede579cde27077aaade46e059558c766
IvayloSlavchev <ivaylo.slavchev124@gmail.com>
2023-02-12 17:32:10 +0200

    Created sidebars for navigation to different pages | Added style to main page in order to render components next to each other | Left sidebar is for navigation to different pages, here I use MUI icons instead of plain text | On the right side is story template, it's only static styles and text for now

---
+++ client/src/components/NavigationBar/Navigation.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Box, ListItem } from '@mui/material'
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import TextsmsOutlinedIcon from '@mui/icons-material/TextsmsOutlined';
+import PeopleIcon from '@mui/icons-material/People';
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
+import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined';
+
+//File will execute automatically
+import '../Styles/NavigationBar.scss';
+
+
+export const NavigationBar = () => {
+    let tokenMap: Map<number, boolean> = new Map();
+
+    //I use any here because it throws an error when trying to set the type to Map<number, boolean>
+    const [mapState, setMapState]: any = React.useState(null);
+
+
+    const IconArray: any = [
+        <HomeOutlinedIcon />,
+        <NotificationsIcon />,
+        <TextsmsOutlinedIcon />,
+        <PeopleIcon />,
+        <SettingsOutlinedIcon />,
+        <LogoutOutlinedIcon className='log-out-icon' />,
+    ]
+
+    function onButtonClick(index: number) {
+        tokenMap.set(index, true);
+        setMapState(tokenMap);
+    }
+
+
+    return (
+        <Box className='navigation-root-element'>
+            <Box className='icon-buttons'>
+                {
+                    IconArray.map((item: any, index: number) => {
+                        return <ListItem key={index} sx={{ background: mapState?.get(index) ? 'rgb(235, 233, 233)' : 'white' }} onClick={() => {
+                            return onButtonClick(index)
+                        }}>{item}</ListItem>
+                    })
+                }
+            </Box>
+        </Box>
+    );
+}
---
+++ client/src/components/StoriesBar/Stories.tsx
@@ -0,0 +1,36 @@
+import { Box, Button } from '@mui/material';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import "../Styles/Stories.scss";
+
+export const Stories = () => {
+    const repeatParagraphElements: Array<object> = [];
+    const predefinedLengthOfElements: number = 5;
+
+    for(let i = 0; i < predefinedLengthOfElements; i++) {
+        repeatParagraphElements.push(
+            //This property should be replaced with person's profile picture
+            <Button className='person-story'>.</Button>
+        );
+    }
+
+    return(
+        <Box className='stories-root-box'>
+           <p className='stories-paragraph'>Stories</p>
+
+           <Box className='story-data-box'>
+                <Box className='add-story-box'>
+                    <Button className='add-story-button'>+ add story</Button>
+                    <Box className='break-line' />
+                    {
+                        repeatParagraphElements.map((item: any, index: number) => {
+                            return <Box className='stories-of-followed-people' key={index}>
+                                {item}
+                            </Box>
+                        })
+                    }
+                    <Box className='expand-icon'><MoreHorizIcon /></Box>
+                </Box>
+           </Box>
+        </Box>
+    );
+}
