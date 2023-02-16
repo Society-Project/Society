dd13214df3cf90e386ef15c5e254b4d55e9e0ccf
di-yordanova <di.gyordanova@gmail.com>
2023-02-16 18:17:52 +0200

    Finalized login page.

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -1,124 +1,102 @@
-import React from 'react'
-import { Box, Button, TextField, Typography } from '@mui/material'
-import laptopPicture from "/src/images/laptopPicture.jpg";
-import headerLogo from "/src/images/headerLogo.png"
-import styled from "styled-components";
-import "../../src/components/Styles/LoginStyles.scss"
-
-
+import React from "react";
+import { Box, Button, TextField } from "@mui/material";
+import laptopPicture from "/src/images/laptopPicture.png";
+import headerLogo from "/src/images/headerLogo.png";
+import "../../src/components/Styles/LoginStyles.scss";
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import InputAdornment from '@mui/material/InputAdornment';
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
 
 const Login = () => {
   return (
-    <div className='wrapper'>
-        <LaptopPicture className="Image" src={laptopPicture.src} alt="Picture" />
+    <Box className="wrapper" margin="-8px">
+      <img className="Image" src={laptopPicture.src} alt="Picture" />
+      <Box
+        className="Form"
+        display="flex"
+        flexDirection={"column"}
+        maxWidth={500}
+        alignItems="center"
+        margin="auto"
+        borderRadius={5}
+        boxShadow={"3px 13px 80px rgba(0, 0, 0, 0.25);"}
+      >
+        <img
+          className="Logo"
+          src={headerLogo.src}
+          width="245px"
+          alt="HeaderLogo"
+        />
+        <TextField
+          className="UsernameField"
+          variant="standard"
+          InputProps={{
+            disableUnderline: true,
+            startAdornment: (
+              <InputAdornment position="start">
+                <PersonOutlineOutlinedIcon />
+              </InputAdornment>
+            ),
+          }}
+          margin="dense"
+          type={"text"}
+          placeholder="username"
+          sx={{
+            borderRadius: 3,
+            backgroundColor: "rgba(74, 122, 99, 0.09)",
+            fontFamily: "Robotto",
+            fontSize: 22,
+            letterSpacing: 0.5,
+            width: 250,
+            height: 48,
+            display: "flex",
+            paddingLeft: 1,
+            justifyContent: "center",
+          }}
+        />       
+          <TextField
+            className="PasswordField"
+            variant="standard"
+            InputProps={{
+              disableUnderline: true,
+              startAdornment: (
+                <InputAdornment position="start">
+                  <LockOutlinedIcon />
+                </InputAdornment>
+              ),
+            }}
+            margin="dense"
+            type={"password"}
+            placeholder="password"
+            sx={{
+              borderRadius: 3,
+              backgroundColor: "rgba(74, 122, 99, 0.09)",
+              fontFamily: "Robotto",
+              fontSize: 22,
+              letterSpacing: 0.5,
+              width: 250,
+              height: 48,
+              display: "flex",
+              paddingLeft: 1,
+              justifyContent: "center",
+            }}
+          />
+        <Button className="LoginButton">Log In</Button>
+        {/* <Button
+          sx={{
+            marginTop: 3,
+            borderRadius: 10,
+            backgroundColor: "white",
+            color: "black",
+          }}
+          variant="contained"
+          color="success"
+        >
+          Sign Up
+        </Button> */}
+      </Box>
+    </Box>
+  );
+};
 
-          <Box className="Form"
-                display="flex" 
-                flexDirection={"column"} 
-                maxWidth={381}
-                alignItems="center" 
-                justifyContent={"center"}
-                margin="auto"
-                borderRadius={5}
-                boxShadow={'3px 13px 80px rgba(0, 0, 0, 0.25);'}    
-                color={"red"} 
-                >
-                {/* <img src='https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1472&q=80' width="500" height="600" alt=''/> */}
-                <Typography 
-                variant="h2" 
-                padding={3} 
-                textAlign='center'
-                >
-                <img src={headerLogo.src} alt="HeaderLogo" 
-                 
-                />
-                  Login
-                  </Typography>
-                <TextField 
-                variant="standard"
-                InputProps={{
-                       disableUnderline: true,
-                     }}
-                margin='dense' 
-                type={'text'} 
-                placeholder='username'
-                sx={{
-                  borderRadius:3, 
-                  backgroundColor:"rgba(74, 122, 99, 0.09)", 
-                  fontFamily: 'Robotto',
-                  fontSize: 22,
-                  letterSpacing: 0.5,
-                  width: 300,
-                  height: 55,
-                  display: 'flex',
-                  alignItems: 'center',
-                  justifyContent: 'center'
-                }}
-                />
-                <TextField 
-                variant="standard"
-                InputProps={{
-                       disableUnderline: true,
-                     }}
-                margin='dense' 
-                type={'password'}  
-                placeholder='password'
-                sx={{
-                  borderRadius:3, 
-                  backgroundColor:"rgba(74, 122, 99, 0.09)", 
-                  fontFamily: 'Robotto',
-                  fontSize: 22,
-                  letterSpacing: 0.5,
-                  width: 300,
-                  height: 55,
-                  display: 'flex',
-                  alignItems: 'center',
-                  justifyContent: 'center'
-                }}
-                />
-                <Button sx={{
-                  marginTop:3, 
-                  borderRadius:10, 
-                  backgroundColor:"white", 
-                  color:"rgba(74, 122, 99, 1)", 
-                  fontFamily: 'Robotto',
-                  fontSize: 20,
-                  letterSpacing: 0.5,
-                  width: 130,
-                  height: 50,                  
-                }}
-                 variant='contained' 
-                 color='success'>
-                    Log In
-                    </Button>
-                    <Button 
-                    sx={{marginTop:3, borderRadius:10, 
-                    backgroundColor:"white",
-                    color:"black",
-                  }}
-                    variant='contained'
-                    color='success'>
-                    Sign Up
-                    </Button>
-          </Box>
-
-    </div>
-  )
-}
-const LaptopPicture = styled.img`
-
-  border-radius: "0% 10% 10% 0%";
-
-  opacity: 0.9;
-
-
-  :hover {
-    cursor: pointer;
-    opacity: 1;
-  }
-}
-`;
-
-export default Login
-
-
+export default Login;
