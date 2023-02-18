4ed95a4dd94910dbede5bf04dd4a6e15296eb3c6
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2023-02-18 22:51:29 +0200

    Import side navigation and stories component

--- client/src/pages/profile/[userId].tsx
+++ client/src/pages/profile/[userId].tsx
@@ -23,6 +23,8 @@ import WorkIcon from "@mui/icons-material/Work";
 import SchoolIcon from "@mui/icons-material/School";
 import TodayIcon from "@mui/icons-material/Today";
 import { Post } from "../../components/posts/Post";
+import { NavigationBar } from "@/components/NavigationBar/Navigation";
+import { Stories } from "@/components/StoriesBar/Stories";
 
 import "./Profile.scss";
 
@@ -86,142 +88,149 @@ const UserProfile = () => {
   };
 
   return (
-    <main className="profile-wrapper">
-      <Container>
-        <Box className="profile-images">
-          <Image
-            src={profileThumbnail}
-            alt="Profile photo"
-            className="profile-banner"
-          />
-          <Box className="profile-picture-box" onClick={handleOpen}>
-            <Box className="profile-image-content">
-              {imageSrc !== "" ? (
-                <Image
-                  src={imageSrc}
-                  alt="Name"
-                  className={`profile-picture ${imageSrc !== "" && "change"}`}
-                  fill
-                />
-              ) : (
-                <Image
-                  src={defaultUserPicture}
-                  alt="Name"
-                  className="profile-picture"
-                />
-              )}
-              <Box className="profile-add-photo">
-                <AddAPhotoIcon />
+    <div className="profile-container">
+      <NavigationBar />
+      <main className="profile-wrapper">
+        <Container>
+          <Box className="profile-images">
+            <Image
+              src={profileThumbnail}
+              alt="Profile photo"
+              className="profile-banner"
+            />
+            <Box className="profile-picture-box" onClick={handleOpen}>
+              <Box className="profile-image-content">
+                {imageSrc !== "" ? (
+                  <Image
+                    src={imageSrc}
+                    alt="Name"
+                    className={`profile-picture ${imageSrc !== "" && "change"}`}
+                    fill
+                  />
+                ) : (
+                  <Image
+                    src={defaultUserPicture}
+                    alt="Name"
+                    className="profile-picture"
+                  />
+                )}
+                <Box className="profile-add-photo">
+                  <AddAPhotoIcon />
+                </Box>
               </Box>
             </Box>
           </Box>
-        </Box>
-        <Box className="profile-user-name">
-          <Typography className="profile-name name">Name Name</Typography>
-          <button className="profile-add-button add-button">
-            <PersonAddAltRoundedIcon />
-            Add friend
-          </button>
-        </Box>
-
-        <Modal
-          open={open}
-          onClose={handleClose}
-          aria-labelledby="modal-modal-title"
-          aria-describedby="modal-modal-description"
-        >
-          <Box className="profile-picture-box">
-            <form
-              method="post"
-              onChange={handleOnChange}
-              onSubmit={handleOnSubmit}
-            >
-              <p>
-                <input type="file" name="file" />
-              </p>
-
-              <Image
-                src={imageSrc !== "" ? imageSrc : defaultUserPicture}
-                alt="User photo"
-                width={400}
-                height={400}
-                className="user-photo-container"
-              />
-
-              {imageSrc && !uploadData && (
-                <p>
-                  <button className="profile-button-upload">
-                    Upload Picture
-                  </button>
-                </p>
-              )}
-
-              {uploadData && (
-                <code>
-                  <pre>{JSON.stringify(uploadData, null, 2)}</pre>
-                </code>
-              )}
-            </form>
+          <Box className="profile-user-name">
+            <Typography className="profile-name name">Name Name</Typography>
+            <button className="profile-add-button add-button">
+              <PersonAddAltRoundedIcon />
+              Add friend
+            </button>
           </Box>
-        </Modal>
 
-        <Grid container spacing={2} className="profile-content">
-          <Grid item xs={4} className="profile-information">
-            <List
-              sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}
-              className="profile-information-data information-data"
-            >
-              <ListItem>
-                <LocationOnIcon />
-                Location
-              </ListItem>
-              <ListItem>
-                <WorkIcon />
-                Work
-              </ListItem>
-              <ListItem>
-                <SchoolIcon />
-                Education
-              </ListItem>
-              <ListItem>
-                <TodayIcon />
-                Birthday
-              </ListItem>
-              <Typography className="profile-edit-link">
-                <Link href="#">Edit</Link>
-              </Typography>
-            </List>
-            <Grid container spacing={2} className="album-grid">
-              {[1, 2, 3, 4].map((_, index) => {
-                return (
-                  <Grid className="album-grid-content" key={index}>
-                    <ListItem className="album-grid-item">
-                      <Image
-                        src={albumPicture}
-                        alt="Profile photo"
-                        className="user-picture-item"
-                      />
-                    </ListItem>
-                    {index === 3 && (
-                      <Box className="photos-plus">
-                        <span className="photos-plus-name">+ photos</span>
-                      </Box>
-                    )}
-                  </Grid>
-                );
-              })}
+          <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+          >
+            <Box className="profile-picture-box">
+              <form
+                method="post"
+                onChange={handleOnChange}
+                onSubmit={handleOnSubmit}
+              >
+                <p>
+                  <input type="file" name="file" />
+                </p>
+
+                <Image
+                  src={imageSrc !== "" ? imageSrc : defaultUserPicture}
+                  alt="User photo"
+                  width={400}
+                  height={400}
+                  className="user-photo-container"
+                />
+
+                {imageSrc && !uploadData && (
+                  <p>
+                    <button className="profile-button-upload">
+                      Upload Picture
+                    </button>
+                  </p>
+                )}
+
+                {uploadData && (
+                  <code>
+                    <pre>{JSON.stringify(uploadData, null, 2)}</pre>
+                  </code>
+                )}
+              </form>
+            </Box>
+          </Modal>
+          <Grid container spacing={2} className="profile-content">
+            <Grid item xs={4} className="profile-information">
+              <List
+                sx={{
+                  width: "100%",
+                  maxWidth: 360,
+                  bgcolor: "background.paper",
+                }}
+                className="profile-information-data information-data"
+              >
+                <ListItem>
+                  <LocationOnIcon />
+                  Location
+                </ListItem>
+                <ListItem>
+                  <WorkIcon />
+                  Work
+                </ListItem>
+                <ListItem>
+                  <SchoolIcon />
+                  Education
+                </ListItem>
+                <ListItem>
+                  <TodayIcon />
+                  Birthday
+                </ListItem>
+                <Typography className="profile-edit-link">
+                  <Link href="#">Edit</Link>
+                </Typography>
+              </List>
+              <Grid container spacing={2} className="album-grid">
+                {[1, 2, 3, 4].map((_, index) => {
+                  return (
+                    <Grid className="album-grid-content" key={index}>
+                      <ListItem className="album-grid-item">
+                        <Image
+                          src={albumPicture}
+                          alt="Profile photo"
+                          className="user-picture-item"
+                        />
+                      </ListItem>
+                      {index === 3 && (
+                        <Box className="photos-plus">
+                          <span className="photos-plus-name">+ photos</span>
+                        </Box>
+                      )}
+                    </Grid>
+                  );
+                })}
+              </Grid>
+            </Grid>
+            <Grid className="profile-action" item xs={8}>
+              <CreatePost className="profile-create-post" />
+              <Box className="profile-publiations">
+                <Post />
+                <Post />
+              </Box>
             </Grid>
           </Grid>
-          <Grid className="profile-action" item xs={8}>
-            <CreatePost className="profile-create-post" />
-            <Box className="profile-publiations">
-              <Post />
-              <Post />
-            </Box>
-          </Grid>
-        </Grid>
-      </Container>
-    </main>
+        </Container>
+      </main>
+      <Stories />
+    </div>
   );
 };
 
