9946a14b68fede948eb38d7854bae84f83b0aa50
IvayloSlavchev <ivaylo.slavchev124@gmail.com>
2023-04-01 17:12:02 +0300

    Fixed endpoints for chat and stories page | Added stories page for desktop screens

---
+++ client/src/pages/chat.tsx
@@ -0,0 +1,7 @@
+import Chat from "@/components/chat/Chat"
+
+const chat = () => {
+  return <Chat />
+}
+
+export default chat
---
+++ client/src/pages/stories.tsx
@@ -0,0 +1,7 @@
+import StoriesPage from "@/components/StoriesPage/StoriesPage"
+
+const stories = () => {
+  return <StoriesPage />
+}
+
+export default stories
