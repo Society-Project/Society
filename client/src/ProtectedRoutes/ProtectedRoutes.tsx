d6017365b7948de26552bd9dd6f27f97153a2002
IvayloSlavchev <ivaylo.slavchev124@gmail.com>
2023-05-05 10:10:47 +0300

    Change variable type from any to string | null

--- client/src/ProtectedRoutes/ProtectedRoutes.tsx
+++ client/src/ProtectedRoutes/ProtectedRoutes.tsx
@@ -2,7 +2,7 @@ import Cookie from 'universal-cookie';
 
 const ProtectedRoutes = ({ children }: any) => {
   const cookies: Cookie = new Cookie();
-  const userCookie: any = cookies.get('accessToken');
+  const userCookie: string | null = cookies.get('accessToken');
  
   if(userCookie === undefined){
     return window.location.href = '/login';
