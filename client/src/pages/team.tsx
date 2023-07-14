a8400d0bed868b62850fb2804fea7a772c50a212
IvayloSlavchev <ivaylo.slavchev124@gmail.com>
2023-07-14 15:47:02 +0300

    Removed Protected Route from Team page

--- client/src/pages/team.tsx
+++ client/src/pages/team.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+
 import styled from "styled-components";
 import { NavigationBar } from "@/components/NavigationBar/Navigation";
 import "../../src/components/Styles/TeamPageStyles.scss";
