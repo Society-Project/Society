74d3234e86682455e3020a5dd1335a4b66dca9f5
IvayloSlavchev <ivaylo.slavchev124@gmail.com>
2023-07-14 15:46:27 +0300

    Removed Protected Route fromTeam page

--- client/src/pages/team.tsx
+++ client/src/pages/team.tsx
@@ -3,15 +3,12 @@ import styled from "styled-components";
 import { NavigationBar } from "@/components/NavigationBar/Navigation";
 import "../../src/components/Styles/TeamPageStyles.scss";
 import { TeamPage } from "@/components/teamPage/TeamPage";
-import ProtectedRoutes from "@/ProtectedRoutes/ProtectedRoutes";
 
 const team = () => {
-  return <ProtectedRoutes>
-     <Main className="main-page">
-      <NavigationBar />
-      <TeamPage />
-    </Main>
-  </ProtectedRoutes>
+  return <Main className="main-page">
+    <NavigationBar />
+    <TeamPage />
+  </Main>
 };
 
 export default team;
